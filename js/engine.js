5db145bf42e470a30b260c875c87076914d1a11a
viktorvm <viktorvm@live.ru>
2017-05-20 12:44:55 +0500

    Edit a task - ready

--- js/engine.js
+++ js/engine.js
@@ -46,10 +46,11 @@ function drawTasks(tasks) {
     let txt = '';
     for (var t in tasks) {
         let status = tasks[t].done ? 'checked' : 'unchecked';
-        txt += '<tr><td><input type="checkbox" onclick="changeTaskStatus(' + t + ')"' + status + '"></td>' +
-                    '<td>' + tasks[t].title + '</td>' +
-					'<td><button type="button">edit</button></td>' +
-					'<td><button type="button" onclick="removeTask(' + t + ')">remove</button></td></tr>';
+        txt += '<tr><td>' + (Number(t) + 1) + '</td>' +
+                    '<td><input type="checkbox" onclick="changeTaskStatus(' + t + ')"' + status + '"></td>' +
+                    '<td><input type="text" id="task' + t + 'Title" value="' + tasks[t].title + '" readonly></td>' +
+					'<td><input type="button" id="task' + t + 'EditBtn" onclick="editTask(' + t + ')" value="edit"></td>' +
+					'<td><input type="button" onclick="removeTask(' + t + ')" value="remove"></td></tr>';
 
     }
     document.getElementById('taskListTable').innerHTML = txt;
@@ -60,10 +61,36 @@ function changeTaskStatus(id) {
     _taskList.tasks[id].changeStatus();
 }
 
+//Remove Task from the List
 function removeTask(id) {
     _taskList.remove(id);
     //redraw a table
     drawTasks(_taskList.tasks);
+}
+
+//Edit Task title when button is clicked
+function editTask(id) {
+    let inp = document.getElementById('task' + id + 'Title');
+    let btn = document.getElementById('task' + id + 'EditBtn');
+    let help = document.getElementById('helpDiv');
+
+    if (inp.readOnly) {
+        inp.readOnly = false;
+        btn.value = 'save';
+        help.innerHTML = 'Press Escape in input to cancel editing Task';
+
+        inp.addEventListener('keydown', function(e) {
+            if (e.keyCode == 27) {
+                inp.readOnly = true;
+                btn.value = 'edit';
+            }
+        });
+    } else {
+        _taskList.tasks[id].editTitle(inp.value);
+        inp.readOnly = true;
+        btn.value = 'edit';
+        help.innerHTML = 'Task ' + (Number(id) + 1) + ' title successfully changed';
+    }
 }
 
 //-->Check if client supports LocalStorage
